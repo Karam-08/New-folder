d920dbcbefe204abd2b12006e6a9c41ced4f026f
Karam Abbas <kma69362@gmail.com>
2025-07-31 14:08:31 -0700

    commit

---
+++ array review.js
@@ -0,0 +1,46 @@
+// Employee Data
+const employees = [
+  { id: 101, name: 'Alice', department: 'Engineering' },
+  { id: 102, name: 'Bob', department: 'Marketing' },
+  { id: 103, name: 'Charlie', department: 'Engineering' },
+  { id: 104, name: 'David', department: 'Sales' }
+];
+
+// Office Supply Inventory 
+const supplies = [
+  'pens',
+  'notebooks',
+  'stapler',
+  'paper'
+];
+
+// Part 1: Adding and removing elements
+
+
+// Task 1.1: New Hire
+// employees.push({ id: 105, name: 'Eve', department: 'Engineering' })
+// console.log(employees)
+
+// Task 1.2: Employee Departure
+// let employee = employees.pop();
+// console.log(employee)
+
+// Task 1.3: Urgent Delivery
+// supplies.unshift('welcome kit')
+// console.log(supplies)
+
+// Task 1.4: Supply Chain Issue
+// supplies.shift();
+// console.log(supplies)
+
+
+// Part 2: Combining and Formatting
+
+
+// Task 2.1: Urgent Supplies
+const new_supplies = ['highlighters', sticky]
+
+// Task 2.2: Supply List for an Email
+
+// Task 2.3: Quick Inspection
+
