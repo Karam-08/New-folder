244f0a302b0bd33d43bd5cf4efaeacb83a92e738
TheProgrammerofalltime <kma69362@gmail.com>
2025-07-31 22:11:52 -0700

    commit

--- array review.js
+++ array review.js
@@ -18,29 +18,47 @@ const supplies = [
 
 
 // Task 1.1: New Hire
-// employees.push({ id: 105, name: 'Eve', department: 'Engineering' })
-// console.log(employees)
+ employees.push({ id: 105, name: 'Eve', department: 'Engineering' })
+ console.log(employees)
 
 // Task 1.2: Employee Departure
-// let employee = employees.pop();
-// console.log(employee)
+ let employee = employees.pop();
+ console.log(employee)
 
 // Task 1.3: Urgent Delivery
-// supplies.unshift('welcome kit')
-// console.log(supplies)
+ supplies.unshift('welcome kit')
+ console.log(supplies)
 
 // Task 1.4: Supply Chain Issue
-// supplies.shift();
-// console.log(supplies)
+ supplies.shift();
+ console.log(supplies)
 
 
 // Part 2: Combining and Formatting
 
 
 // Task 2.1: Urgent Supplies
-const new_supplies = ['highlighters', sticky]
+const newSupplies = ['highlighters', 'sticky notes']
+const fullSupplies = newSupplies.concat(supplies)
+console.log(fullSupplies)
 
 // Task 2.2: Supply List for an Email
+let emailSuppliesList = fullSupplies.join(", ") 
+console.log(emailSuppliesList)
 
 // Task 2.3: Quick Inspection
+console.log(newSupplies.toString())
+// It seems that .toString() only puts a comma between while .join() lets you put whatever between
+
+
+// Part 3: Slicing and Findingz
+
+
+// Task 3.1: Engineering Team
+
+// Task 3.2: First Three Employees
+const top3 = employees.slice(1, 2, 3)
+console.log(top3)
+
+// Task 3.3: Alphabetical Order
 
